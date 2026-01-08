59c9d9c44017714a1c8ba3c1610b92fccb6724c3
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-08 20:27:01 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+with open('README.md','r') as f:
+    long_description = f.read()
+
+with open('LICENSE','r') as f:
+    license = f.read()
+
+setup(name='airnet',
+      version='0.1.0',
+      description='Forecasting net for atmospherical particulate matter of 2.5 microns or less',
+      long_description=long_description,
+      license=license,
+      author='Alexandros Ntigkaris',
+      url='https://github.com/ntigkaris/airnet',
+      packages=['airnet'],
+      install_requires=['numpy','pandas','openpyxl','matplotlib','scikit-learn','torch'])
