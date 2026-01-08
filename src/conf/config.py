071994f237f83732501273fc9beb908ef125af1d
ntigkaris <87975862+ntigkaris@users.noreply.github.com>
2026-01-08 20:21:48 +0200

    Create config.py

---
+++ src/conf/config.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+
+class cfg:
+    rawdir = 'data/raw'
+    prodir = 'data/processed'
+    pthdir = 'data/model'
+    logdir = 'data/logs'
+    outdir = 'data/output'
+    timestamp = datetime.today().strftime('%Y%m%d%H%M%S')
+    features = ['pm25','pm10_l1']
+    target = ['pm25']
+    knn = 2
+    trnsize = .9
+    nfolds = 2
+    seed = 42
+    idim = len(features)
+    odim = len(target)
+    hdim = 100
+    wsize = 4
+    bsize = 8
+    loader = {'batch_size':bsize,
+              'shuffle':False,
+              'pin_memory':True,
+              'drop_last':True}
+    lr = 1e-4
+    momentum = .5
+    delta = 1.
+    epochs = 4
